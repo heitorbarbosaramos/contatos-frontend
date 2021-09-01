26e7516565887f3a7a32957349e2561db9ca3107
heitor.ramos <heitorhfbr@gmail.com>
2021-08-31 23:08:11 -0300

    Aplicando alguns styles iniciais para o headers

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,16 @@
 <template>
-    <div>
-    </div>
+    <header>
+
+        <nav  class="container">
+        
+            <a href="#">
+                <img alt="Logotipo" id="logo" src="https://thumbs.jusbr.com/filters:format(webp)/imgs.jusbr.com/publications/images/fdec4032a25557564976374b8f6ba6bb">
+            </a>
+
+            <img alt="menu mobile" id="menu-button" src="https://img.icons8.com/cotton/2x/4a90e2/menu.png"/>
+
+        </nav>
+    </header>
 </template>
 
 <script>
@@ -10,5 +20,25 @@ export default {
 </script>
 
 <style scoped>
+    header{
+        background-color: var(--color-background-nav);
+        height: 60px;
+        width: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
 
+    nav{
+        display: flex;
+        justify-content: space-between;
+    }
+
+    #logo{
+        width: 130px;
+    }
+
+    #menu-button{
+        width: 60px;
+    }
 </style>
