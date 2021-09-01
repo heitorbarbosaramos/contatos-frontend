bdebb47e872a76bbfac592fdfeb9a4caadaca323
heitor.ramos <heitorhfbr@gmail.com>
2021-09-01 14:09:11 -0300

    Estilizando componente Footer

--- src/components/pages/Footer.vue
+++ src/components/pages/Footer.vue
@@ -1,12 +1,30 @@
 <template>
 
-    <footer></footer>
+    <footer>
+        <a class="socialLink" v-for="socialLink in socialLinks" :key="socialLink.id" :href="socialLink.link" target="_blank">
+            <img :alt="socialLink.label" :tile="socialLink.label" :src="socialLink.icon" />
+        </a>
+    </footer>
 
 </template>
 
 <script>
+
+import api from '@/services/ap.js';
+
 export default({
-    name: 'Footer'
+    name: 'Footer',
+    data(){
+        return{
+            socialLinks:[]
+        }
+    },
+    mounted(){
+        api.get('/social/all').then(response => {
+            console.log(response.data);
+            this.socialLinks = response.data;
+        })
+    }
 })
 </script>
 
@@ -19,5 +37,20 @@ export default({
         display: flex;
         justify-content: center;
         justify-items: center;
+    }
+
+    .socialLink{
+        border: 2px solid var(--color-text-light);
+        display: flex;
+        justify-content: center;
+        align-content: center;
+        border-radius: 50%;
+        width: 40px;
+        height: 40px;
+        margin-top: 10px;
+        margin-left: 10px;
+    }
+    .socialLink img{
+        width: 20px;
     }
 </style>
