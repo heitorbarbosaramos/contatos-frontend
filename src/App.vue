4190ba10d0d90587ee8d6afd5686dc795d535cec
heitor.ramos <heitorhfbr@gmail.com>
2021-09-01 10:54:10 -0300

    Criando componente Footer

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div id="app">
     <Header />
     <Home />
+    <Footer />
 
   </div>
 </template>
@@ -11,11 +12,14 @@
 import './styles/global.css';
 import Header from './components/Header.vue';
 import Home from './components/pages/Home';
+import Footer from './components/pages/Footer';
+
 export default {
   name: 'App',
   components: {
     Header,
     Home,
+    Footer
 }
 }
 </script>
---
+++ src/components/pages/Footer.vue
@@ -0,0 +1,23 @@
+<template>
+
+    <footer></footer>
+
+</template>
+
+<script>
+export default({
+    name: 'Footer'
+})
+</script>
+
+<style scoped>
+
+    footer{
+        height: 60px;
+        width: 100%;
+        background-color: var(--color-background-nav);
+        display: flex;
+        justify-content: center;
+        justify-items: center;
+    }
+</style>
