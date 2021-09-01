017c7fa0d5b2de135b078fdf6fd02262d32620fe
heitor.ramos <heitorhfbr@gmail.com>
2021-08-31 23:42:48 -0300

    Estilizando menu

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -9,6 +9,16 @@
 
             <img alt="menu mobile" id="menu-button" src="https://img.icons8.com/cotton/2x/4a90e2/menu.png"/>
 
+            <div id="menu-overlay"></div>
+
+            <div id="menu-items">
+                <img alt="Logotipo" id="menu-logo" src="https://thumbs.jusbr.com/filters:format(webp)/imgs.jusbr.com/publications/images/fdec4032a25557564976374b8f6ba6bb">
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Contatos</a></li>
+                    <li><a href="#">Sobre</a></li>
+                </ul>
+            </div>
         </nav>
     </header>
 </template>
@@ -40,5 +50,47 @@ export default {
 
     #menu-button{
         width: 60px;
+    }
+
+    #menu-overlay{
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 40%;
+        height: 100vh;
+        background-color: rgb(0, 0, 0);
+        opacity: 0.8;
+    }
+
+    #menu-logo{
+        width: 110px;
+        margin-top: 30px;
+        margin-bottom: 30px;
+    }
+
+    #menu-items{
+        position: fixed;
+        top: 0;
+        right: 0;
+        background-color: var(--color-background-nav);
+        width: 60%;
+        height: 100vh;
+        display: flex;
+        flex-direction: column;
+        justify-content: start;
+        align-items: center;
+    }
+
+    ul{
+        list-style: none;
+    }
+
+    ul li{
+        margin: 20px 0px;
+        text-align: center;
+    }
+
+    ul li a{
+        color: var(--color-text-light);
     }
 </style>
